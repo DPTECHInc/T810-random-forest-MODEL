49a553fb923a30358d1a95ef4447ae343ad36207
DPTECHINC <dayle.parent.dev@gmail.com>
2024-05-05 16:01:19 +0200

    Feat: base project

---
+++ src/classes/data_loader.py
@@ -0,0 +1,29 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+class DataLoader:
+    def __init__(self, directory_path, image_size=(64, 64), batch_size=500):
+        self.directory_path = directory_path
+        self.image_size = image_size
+        self.batch_size = batch_size
+
+    def load_images(self):
+        datagen = ImageDataGenerator(rescale=1./255)
+        generator = datagen.flow_from_directory(
+            directory=self.directory_path,
+            target_size=self.image_size,
+            batch_size=self.batch_size,
+            class_mode='binary',
+            shuffle=False
+        )
+        images, labels = next(generator)
+        return images, labels
+    def load_test_images(self, test_path):
+        datagen = ImageDataGenerator(rescale=1./255)
+        generator = datagen.flow_from_directory(
+            directory=test_path,
+            target_size=(64, 64),  # Assurez-vous que la taille cible correspond à vos besoins
+            batch_size=32,
+            class_mode='binary',  # ou 'categorical' si plus de deux classes
+            shuffle=False
+    )
+        return generator
---
+++ src/classes/feature_extractor.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+class FeatureExtractor:
+    def extract_features(self, images):
+        features = images.mean(axis=(1, 2, 3))
+        return features
