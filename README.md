5d58b159b7ec1baea082debc32031a21b7e8fbb0
barbu <dayle.parent.dev@gmail.com>
2024-05-06 11:57:19 +0200

    feat: syntax, indentations

--- README.md
+++
@@ -1,1 +0,0 @@
-# T810-ForestModel
--- src/classes/feature_extractor.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-class FeatureExtractor:
-    def extract_features(self, images):
-        features = images.mean(axis=(1, 2, 3))
-        return features
